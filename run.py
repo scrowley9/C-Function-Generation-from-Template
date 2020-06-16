c91dbeee24709d0b7fc555f874e2469221cec25a
Sean <scrowley9@gatech.edu>
2020-06-16 16:58:08 -0400

    read JSON

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import json
 import argparse
-import template
+from template import Template
 
 def write_functions_to_C_file(functions, c_filepath):
     print("Write function to C file")
@@ -15,19 +15,24 @@ def write_function_prototypes_to_H_file(functions, h_filepath):
 # Place function Prototypes in associated header 
 
 def main():
-    # # Setup to Receive Command Line Args
-    # parser = argparse.ArgumentParser(description="Generate Device Hex Code")
-    # parser.add_argument("--template_json", action="store", dest="template_json", required=True, help="Provide the path of the template funtions JSON")
+    # Setup to Receive Command Line Args
+    parser = argparse.ArgumentParser(description="Build C Functions from Templates")
+    parser.add_argument("--json", action="store", dest="template_json", required=True, help="Provide the path of the template funtions JSON")
 
-    # # Args list
-    # given_args = parser.parse_args()
+    # Args list
+    given_args = parser.parse_args()
     
-    # # Extract each argument
-    # build = given_args.build
-    # rand_path = given_args.rand_path
-    # config_path = given_args.config_path
-    Template()
-    print("Hello")
+    # Extract each argument
+    json_path = given_args.template_json
+
+    # Retreive JSON data
+    with open(json_path) as template_file:
+        template_data = json.load(template_file)
+
+    # TODO: conver JSON data into appropriate objects
+    print(template_data)
+
+    temp = Template("ret", "arg", "hello")
 
 if __name__ == "__main__":
     main()
